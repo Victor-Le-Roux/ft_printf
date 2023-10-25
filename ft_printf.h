a5733c6663a79981a0b690d83d0c7edbe6c0679c
Victor Le Roux <vle-roux@c1r1p9.42lehavre.fr>
2023-10-25 05:27:32 +0200

    projet mis a jour

--- ft_printf.h
+++ ft_printf.h
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vle-roux <vle-roux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 03:03:20 by vle-roux          #+#    #+#             */
-/*   Updated: 2023/10/25 04:25:05 by vle-roux         ###   ########.fr       */
+/*   Created: 2023/10/25 03:56:07 by vle-roux          #+#    #+#             */
+/*   Updated: 2023/10/25 04:06:57 by vle-roux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-int	ft_printf(const char *str, ...)
-{
-	int	i;
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <stdio.h>
 
-	i = 0;
-	while (str[i])
-	{
-		if(str[i+1] == '%' && str[i] == '%')
-			i++;
-		write(1, &str[i], 1);
-		i++;
-	}
-	return (0);
-}
 
-int	main(void)
-{
-	ft_printf("Ceci est un exemple de pourcentage : %%\n");
-	return (0);
-}
+#endif
